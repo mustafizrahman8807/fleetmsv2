416833c89fc9b5128f9e5d801a5927a2ba9e5d2e
Mustafijur Rahman <mustafizrahman8807@gmail.com>
2022-10-01 20:20:19 +0000

    first commit

---
+++ src/main/java/com/kindsonthegenius/fleetmsv2/ApplicationController.java
@@ -0,0 +1,21 @@
+package com.kindsonthegenius.fleetmsv2;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/widgets")
+    public String widgets() {
+        return "widgets";
+    }
+
+
+}
---
+++ src/main/java/com/kindsonthegenius/fleetmsv2/Fleetmsv2fApplication.java
@@ -0,0 +1,13 @@
+package com.kindsonthegenius.fleetmsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fleetmsv2fApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fleetmsv2fApplication.class, args);
+    }
+
+}
